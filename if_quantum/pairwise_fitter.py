f3e88d898d07f0b89c04fbae7527d1eec5a8efd1
Matteo Rossi <teo.red90@gmail.com>
2019-09-13 19:06:03 +0200

    Some comments and refactoring

--- if_quantum/pairwise_fitter.py
+++ if_quantum/pairwise_fitter.py
@@ -33,24 +33,23 @@ class PairwiseStateTomographyFitter(StateTomographyFitter):
         3. Relabel the circuits so that they are acceptable for the StateTomographyFitter
         """
 
+        # If no list of pairs provided, then evaluate for all qubit pairs
         if not pairs_list:
             pairs_list = list(combinations(self._qubit_list, 2))
-        
+
         result = {}
 
         for p in pairs_list:
-            rho = self.fit_ij(*p)
-            result[p] = rho
+            result[p] = self.fit_ij(*p)
 
         return result
-            
 
     def fit_ij(self, i, j):
         """
-
+            Returns the tomographic reconstruction for the qubits i and j
         """
         assert i != j, "i and j must be different"
-        
+
         # Get the layer of interest in the list of circuits
         l = self._find_layer(i, j)
 
@@ -58,30 +57,41 @@ class PairwiseStateTomographyFitter(StateTomographyFitter):
         circuits = self._circuits[0:3]
         circuits += self._circuits[(3 + 6*l) : (3 + 6*(l+1))]
 
-
         # This will create an empty _data dict for the fit function
+        # We are using a member field so that  we can use the super() fit 
+        # function
         self._data = {}
 
         # Process measurement counts into probabilities
         for circ in circuits:
+            # Take only the relevant qubit labels from the circuit label
             tup = literal_eval(circ.name)
             tup = (tup[i], tup[j])
+
+            # Marginalize the counts for the two relevant qubits
             counts = marginal_counts(self._result.get_counts(circ), [i, j])
 
-            # Populate the data 
+            # Populate the data
             self._data[tup] = counts
 
+        # Test that all the required measurements are there
         expected_corr = product(['X', 'Y', 'Z'], ['X', 'Y', 'Z'])
         if set(self._data.keys()) != set(expected_corr):
             raise Exception("Could not find all the measurements required for tomography")
 
+        # Do the actual fit
         result = super().fit()
+
+        # clear the _data field
         self._data = None
         return result
 
     def _find_layer(self, i, j):
+        """
+        Utility function for finding the position of the circuits in the circuit
+        list returned by pairwise_state_tomography()
+        """
         l = 0
-        while(int(i/3**l) % 3 == int(j/3**l) % 3):
+        while int(i/3**l) % 3 == int(j/3**l) % 3:
             l += 1
         return l
-
