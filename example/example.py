1126a20c358f89e8bbf6573a15d19ed4fd27b37f
Matteo Rossi <teo.red90@gmail.com>
2019-09-13 16:29:19 +0200

    Example

---
+++ example/example.py
@@ -0,0 +1,32 @@
+"""Ciaone"""
+import sys
+sys.path.append("..") 
+
+from if_quantum.pairwise_state_tomography_circuits import pairwise_state_tomography_circuits
+from if_quantum.pairwise_fitter import PairwiseStateTomographyFitter
+
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import execute
+from qiskit import Aer
+
+import numpy as np
+
+q = QuantumRegister(3)
+qc = QuantumCircuit(q)
+
+qc.h(q[0])
+qc.cx(q[0], q[1])
+
+circ = pairwise_state_tomography_circuits(qc, [0, 1, 2])
+
+job = execute(circ, Aer.get_backend("qasm_simulator"), shots=20000)
+
+fitter = PairwiseStateTomographyFitter(job.result(), circ, [0, 1, 2])
+
+np.set_printoptions(suppress=True)
+
+print(fitter.fitij(0, 1))
+
+print(fitter.fitij(1, 2))
+
+print(fitter.fitij(0, 2))
