2cd8702f4c63a838ca10eb2eaaa73819320bef62
Matteo Rossi <teo.red90@gmail.com>
2019-09-13 14:03:48 +0200

    Structure for fit function

--- if-quantum/pairwise_fitter.py
+++ if-quantum/pairwise_fitter.py
@@ -5,4 +5,16 @@ def PairwiseStateTomographyFitter(StateTomographyFitter):
     Doc
     """
 
-    
+    def __init__():
+
+
+    def fit(pairs_list):
+        """
+        pair_list = [ [ i1, j1], [i2, j2], ... ]
+
+        1. Select the results and circuits that are relevant for the qubit pairs
+        2. Prepare marginalized results (using the builtin function)
+        3. Relabel the circuits so that they are acceptable for the StateTomographyFitter
+        """
+
+        
